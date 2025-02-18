74f0ca2c3a26c251bdcc6da59aee5bd1e55a4071
Arjun Dabir <arjunadabir@gmail.com>
2025-02-17 16:00:38 -0800

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,6 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <GoogleAnalytics gaId="G-DSQEZ2WD77" />
     </html>
   );
 }

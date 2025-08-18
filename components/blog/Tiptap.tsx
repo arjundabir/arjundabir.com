15b610c06e48746548b3d4237ea5832da5ae7508
Arjun Dabir <arjunadabir@gmail.com>
2025-08-17 22:59:17 -0400

    configured code block

--- components/blog/Tiptap.tsx
+++ components/blog/Tiptap.tsx
@@ -2,11 +2,32 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { all, createLowlight } from "lowlight";
+import "highlight.js/styles/github.min.css";
 
 const Tiptap = () => {
+  const lowlight = createLowlight(all);
+
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<h1>Hello World! 🌎️</h1>",
+    extensions: [
+      CodeBlockLowlight.configure({
+        lowlight,
+        defaultLanguage: "javascript",
+        HTMLAttributes: {
+          class: "mb-4",
+        },
+      }),
+      StarterKit.configure({
+        codeBlock: false,
+        heading: {
+          levels: [1, 2],
+        },
+      }),
+    ],
+    content: `<h1>Hello World! 🌎️</h1>
+    <pre><code>function helloWorld(){}</code></pre>
+    `,
     immediatelyRender: false,
     autofocus: true,
     editorProps: {

0dd8f073ebafb9ebd3777228766ed23abc2b1687
Arjun Dabir <arjunadabir@gmail.com>
2025-08-28 12:25:42 -0400

    database migration

--- drizzle/0000_brave_goblin_queen.sql
+++ drizzle/0000_brave_goblin_queen.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "blogs" (
-	"slug" varchar(32) PRIMARY KEY NOT NULL,
-	"date" varchar(8) NOT NULL,
+	"slug" varchar(8) PRIMARY KEY NOT NULL,
+	"date" varchar(10) NOT NULL,
 	"title" text DEFAULT 'Hello World' NOT NULL,
 	"type" text DEFAULT 'drafts' NOT NULL,
 	"content" jsonb
